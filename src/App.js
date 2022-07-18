73bc0b786391e8ac95ab7fe5fae512c01b268a37
Vipin pathak <vipinpathaka@gmail.com>
2022-07-18 13:14:12 +0530

    6th commit

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import React, { Suspense, lazy} from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 // import Home from './components/Home';
-import Slides from './components/Slides';
+// import Slides from './components/Slides';
 
-const Home = lazy(()=>import('./components/Home'))
+const Home = lazy(()=>import('./components/Home'));
+const Slides = lazy(()=>import('./components/Slides'))
 
 function App() {
   return (
